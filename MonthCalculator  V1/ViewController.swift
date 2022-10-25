eb27a5e61b5ca053891818a1f63563ffd0862652
@luciano.savi <luciano.savi@foursys.com.br>
2022-10-25 05:53:51 -0700

    Initial Commit

---
+++ MonthCalculator  V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonthCalculator  V1
+//
+//  Created by user220270 on 10/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
